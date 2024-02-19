ca47a037d367654b3b3bf40ae00a7911d942539c
Sreeja Veerappan <123928030+Sreejaveer@users.noreply.github.com>
2024-02-19 16:29:14 +0530

    Add files via upload

---
+++ prg1.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+{
+int main()
+int a,b,s;
+cout<<"Enter the number";
+cin>>a;
+cout<<"Enter 2nd number:";
+cin<<b;
+s=a+b;
+cout<<"The sum is"<<s;
+return 0;
+}
